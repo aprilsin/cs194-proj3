7e3d348c0b1c1786b40b4598a25313b10fcf2db7
rainbowCat <jazziblue@gmail.com>
2020-10-11 02:46:27 -0700

    Clean up alignment.

--- utils.py
+++ utils.py
@@ -74,10 +74,11 @@ def read_img(img_name) -> np.ndarray:
     return img
 
 
-def check(img) -> None:
+def check_img_type(img) -> None:
     """ Check image data type """
     assert img.dtype == "float64"
-    assert np.max(img) == 1.0 and np.min(img) == 0.0
+    print(np.max(img), np.min(img))
+    assert np.max(img) <= 1.0 and np.min(img) >= 0.0
 
 
 #######################
@@ -115,21 +116,17 @@ def align(
         scaled = img
 
     scaled_h, scaled_w = scaled.shape[0], scaled.shape[1]
-    print("scaled shape ", scaled.shape)
     # do crop
     col_center, row_center = find_centers(left_eye * scale, right_eye * scale)
+    row_center += 50
 
-    col_shift = int(target_w // 2)  # + DEFAULT_HEIGHT * 0.12
-    row_shift = int(target_h // 2)  # + DEFAULT_WIDTH * 0.1
+    col_shift = int(target_w // 2)
+    row_shift = int(target_h // 2)
 
     col_start = col_center - col_shift
     col_end = col_center + col_shift
     row_start = row_center - row_shift
     row_end = row_center + row_shift
-
-    print("center ", col_center, row_center)
-    print("shift ", col_shift, row_shift)
-    print("before pad index ", row_start, row_end, col_start, col_end)
 
     rpad_before, rpad_after, cpad_before, cpad_after = 0, 0, 0, 0
     if row_start < 0:
@@ -151,101 +148,11 @@ def align(
         mode=PAD_MODE,
     )
 
-    print("padded shape ", padded.shape)
-    print("index ", row_start, row_end, col_start, col_end)
     assert row_start >= 0 and row_end >= 0 and col_start >= 0 and col_end >= 0
     cropped = padded[row_start:row_end, col_start:col_end, :]
-    # check(scaled)
-    print("cropped shape ", cropped.shape)
-    return scaled
-
-    # def crop_img(img: np.ndarray, pts) -> np.ndarray:
-    #     left, right, top, down = pts
-    #     eye_len = np.sqrt((right[1] - left[1]) ** 2 + (right[0] - left[0]) ** 2)
-
-    #     col_start = cx - DEFAULT_WIDTH * scale // 2
-    #     col_end = cx + DEFAULT_WIDTH * scale // 2
-    #     row_start = cy - DEFAULT_HEIGHT * scale // 2
-    #     row_end = cy + DEFAULT_HEIGHT * scale // 2
-
-    #     print(row_start, row_end, col_start, col_end)
-    #     return img[row_start:row_end, col_start:col_end, :]
-
-    # def recenter(im, r, c):
-    #     R, C, _ = im.shape
-    #     rpad = (int)(np.abs(2 * r + 1 - R))
-    #     cpad = (int)(np.abs(2 * c + 1 - C))
-    #     return np.pad(
-    #         im,
-    #         [
-    #             (0 if r > (R - 1) / 2 else rpad, 0 if r < (R - 1) / 2 else rpad),
-    #             (0 if c > (C - 1) / 2 else cpad, 0 if c < (C - 1) / 2 else cpad),
-    #             (0, 0),
-    #         ],
-    #         mode=PAD_MODE,
-    #     )
-
-    # def align_image_centers(im1, im2, pts):
-    #     p1, p2, p3, p4 = pts
-    #     h1, w1, c1 = im1.shape
-    #     h2, w2, c2 = im2.shape
-
-    #     cx1, cy1 = find_centers(p1, p2)
-    #     cx2, cy2 = find_centers(p3, p4)
-
-    #     im1 = recenter(im1, cy1, cx1)
-    #     im2 = recenter(im2, cy2, cx2)
-    #     return im1, im2
-
-    # def rescale_images(im1, im2, pts):
-    #     p1, p2, p3, p4 = pts
-    #     print("65:", im1.dtype, im2.dtype)
-    #     # distance formula
-    #     len1 = np.sqrt((p2[1] - p1[1]) ** 2 + (p2[0] - p1[0]) ** 2)
-    #     len2 = np.sqrt((p4[1] - p3[1]) ** 2 + (p4[0] - p3[0]) ** 2)
-
-    #     dscale = len2 / len1
-    #     if dscale < 1:
-    #         im1 = transform.rescale(
-    #             im1, dscale, preserve_range=True, multichannel=True, mode=PAD_MODE
-    #         )
-    #     else:
-    #         im2 = transform.rescale(
-    #             im2, 1.0 / dscale, preserve_range=True, multichannel=True, mode=PAD_MODE
-    #         )
-    #     #     print("image shapes: ", im1.shape, im2.shape)
-    #     return im1, im2
-
-    # def match_img_size(im1, im2):
-    #     # Make images the same size
-    #     h1, w1, c1 = im1.shape
-    #     h2, w2, c2 = im2.shape
-    #     assert c1 == c2
-
-    #     if h1 < h2:
-    #         im2 = im2[int(np.floor((h2 - h1) / 2.0)) : -int(np.ceil((h2 - h1) / 2.0)), :, :]
-    #     elif h1 > h2:
-    #         im1 = im1[int(np.floor((h1 - h2) / 2.0)) : -int(np.ceil((h1 - h2) / 2.0)), :, :]
-    #     if w1 < w2:
-    #         im2 = im2[:, int(np.floor((w2 - w1) / 2.0)) : -int(np.ceil((w2 - w1) / 2.0)), :]
-    #     elif w1 > w2:
-    #         im1 = im1[:, int(np.floor((w1 - w2) / 2.0)) : -int(np.ceil((w1 - w2) / 2.0)), :]
-    #     print("image shapes: ", im1.shape, im2.shape)
-    #     assert im1.shape == im2.shape
-    #     return im1, im2
-
-    # def align_images(im1, im2, im1_pts, im2_pts):
-    #     """ Align two images given two points for alignment """
-    #     h1, w1, c1 = im1.shape
-    #     h2, w2, c2 = im2.shape
-    #     print("113:", im1.dtype, im2.dtype)
-    #     assert c1 == c2
-    #     print("asserted channels")
-    #     pts = [im1_pts[0], im1_pts[1], im2_pts[0], im2_pts[1]]
-    #     im1, im2 = align_image_centers(im1, im2, pts)
-    #     im1, im2 = rescale_images(im1, im2, pts)
-    #     im1, im2 = match_img_size(im1, im2)
-    #     return im1, im2
+    assert cropped.shape[0] == DEFAULT_HEIGHT and cropped.shape[1] == DEFAULT_WIDTH
+    check_img_type(cropped)
+    return cropped
 
 
 #######################
