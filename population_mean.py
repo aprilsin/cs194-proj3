8c543dba094a922096a2aca60fb0a7088c68ab74
rainbowCat <jazziblue@gmail.com>
2020-10-11 23:16:57 -0700

    Add input files

--- population_mean.py
+++ population_mean.py
@@ -4,10 +4,10 @@ import morph
 def compute_population_mean(population_imgs: np.ndarray, population_pts: np.ndarray) ->np.ndarray:
     assert len(population_imgs) == len(population_pts)
     assert all(assert_img_type(img) for img in population_imgs)
-    mean_points = np.mean(population_pts)
+    
     mean_img = population_imgs[0]
-    for img in population_imgs[1:]:
-        morph.compute_middle_object()
+
+    mean_pts = np.mean(population_pts, axis=0)
 
 def load_points_from_asf(file_name):
     points = np.genfromtxt(file_name)
